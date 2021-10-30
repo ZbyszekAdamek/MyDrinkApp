d71b9a380777697b4318ab7bbbf79977840929a2
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-10-30 09:44:23 +0200

    Nowy Projekt

---
+++ src/main/java/pl/coderslab/MyDrinkApp/service/MyUserDetailsService.java
@@ -0,0 +1,4 @@
+package pl.coderslab.MyDrinkApp.service;
+
+public class MyUserDetailsService {
+}
---
+++ src/test/java/pl/coderslab/MyDrinkApp/DrinkappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.MyDrinkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
